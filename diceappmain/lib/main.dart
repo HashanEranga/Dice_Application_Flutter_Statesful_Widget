6fdccd705b7b0b4a0401c7b56077a9f42439d2a4
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-07-25 01:46:13 +0530

    Build(Two Dice application) : Stateful and less

--- diceappmain/lib/main.dart
+++ diceappmain/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -9,8 +10,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Text("Initial Setup"),
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Center(
+            child: Text("Dice Application"),
+          ),
+        ),
+        body: DicePage(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceLeftFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      if (leftDiceNumber > 6) {
+        leftDiceNumber = 1;
+      }
+      print('Left Dice number = $leftDiceNumber');
+    });
+  }
+
+  void changeDiceRightFace() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      if (rightDiceNumber > 6) {
+        rightDiceNumber = 1;
+      }
+      print('Right Dice Number = $rightDiceNumber');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceLeftFace();
+                },
+                child: Image.asset('lib/images/Alea_$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceRightFace();
+                },
+                child: Image.asset('lib/images/Alea_$rightDiceNumber.png'),
+              ),
+            )
+          ],
         ),
       ),
     );
